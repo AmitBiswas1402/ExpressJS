e4c26d9725c656d29d2649a089529fee39b6a6e5
Amit Biswas <amit.142biswas@gmail.com>
2024-01-31 19:03:57 +0530

    built logic through controllers

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -3,6 +3,8 @@ class ApiResponse {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
-        this.success = success;
+        this.success = success < 400;
     }
 }
+
+export { ApiResponse }
