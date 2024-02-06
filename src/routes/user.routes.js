94fd13ec4fbbc2ab405df3537324be25916accd1
Amit Biswas <amit.142biswas@gmail.com>
2024-02-06 18:03:26 +0530

    comment, like, playlist, tweet model added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,8 @@ import {
     refreshAccessToken, 
     changeCurrenPassword, 
     getCurrentUser, 
-    updateAccountDetails, 
+    updateAccountDetails,
+    updateUserCoverImage, 
     updateUserAvatar, 
     getUserChannelProfile, 
     getWatchHistory 
@@ -39,7 +40,7 @@ router.route("/current-user").post(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
 router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 router.route("/history").get(verifyJWT, getWatchHistory)
